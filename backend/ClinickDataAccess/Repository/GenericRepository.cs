825e8d8f6b848fe5bc0ce337a20fa405c180e1f8
Abdül Samed Kara <abdulsamedkra@gmail.com>
2025-10-26 00:13:33 +0300

    Genel repository deseni uygulaması eklendi
    Entity Framework kullanarak varlıklar (entities) için yeniden kullanılabilir CRUD (Oluşturma, Okuma, Güncelleme, Silme) işlemleri sağlayan IGenericRepository arayüzü ve GenericRepository sınıfı tanıtıldı.
    Bu yapı, veri erişim mantığında kodun bakımını kolaylaştırır ve tekrar eden kodları azaltır.

---
+++ backend/ClinickDataAccess/Repository/GenericRepository.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinickDataAccess.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected readonly DatabaseBaglanti _dbBaglanti;
+        private readonly DbSet<T> _dbSet;
+        public GenericRepository(DatabaseBaglanti dbBaglanti)
+        {
+            _dbBaglanti = dbBaglanti;
+            _dbSet = _dbBaglanti.Set<T>();
+        }
+        public void Create(T entity)
+        {
+            _dbSet.Add(entity);
+            _dbBaglanti.SaveChanges();
+        }
+
+        public void Delete(T entity)
+        {
+            _dbSet.Remove(entity);
+            _dbBaglanti.SaveChanges();
+        }
+
+        public void DeleteByRange(List<T> entities)
+        {
+            _dbSet.RemoveRange(entities);
+            _dbBaglanti.SaveChanges();
+        }
+
+        public IQueryable<T> GetAll()
+        {
+           return _dbSet.AsQueryable();
+        }
+
+        public T GetById(int id)
+        {
+            return _dbSet.Find(id);
+
+        }
+
+        public void Update(T entity)
+        {
+            _dbSet.Update(entity);
+            _dbBaglanti.SaveChanges();
+        }
+    }
+}
---
+++ backend/ClinickDataAccess/Repository/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinickDataAccess.Repository
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        #region CRUD
+        void Create(T entity);
+        IQueryable<T> GetAll();
+        T GetById(int id);
+        void Update(T entity);
+        void Delete(T entity);
+        void DeleteByRange(List<T> entities);
+        #endregion
+    }
+}
