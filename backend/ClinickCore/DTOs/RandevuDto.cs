2783d2faedc63413f03e5a5cb0d87fa33665d975
Abdül Samed Kara <abdulsamedkra@gmail.com>
2025-10-28 19:09:44 +0300

    DTO’lar Eklendi ve Entity İsim Alanları Yeniden Düzenlendi
    Doktor, Hasta, Kullanıcı, Randevu ve Uzmanlık için DTO sınıfları eklenerek create ve update işlemleri desteklendi. Entity sınıfları yeni Entities isim alanına taşındı ve tüm referanslar buna göre güncellendi. Bu düzenleme, proje yapısının daha organize ve anlaşılır olmasını sağladı.

---
+++ backend/ClinickCore/DTOs/RandevuDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinickCore.DTOs
+{
+    public class RandevuOlusturDto
+    {
+        public int HastaId { get; set; }
+        public int DoktorId { get; set; }
+        public DateTime RandevuTarihi { get; set; }
+        public string HastaŞikayeti { get; set; }
+    }
+
+    public class RandevuGüncelleDto
+    {
+        public int RandevuId { get; set; }
+        public DateTime? RandevuTarihi { get; set; }
+        public string? HastaŞikayeti { get; set; }
+        public string? Durum { get; set; }
+        public string? DoktorNotları { get; set; }
+    }
+}
---
+++ backend/ClinickCore/DTOs/UzmanlıkDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinickCore.DTOs
+{
+    public class UzmanlıkOlusturDto
+    {
+        public string UzmanlıkAdı { get; set; }
+    }
+    public class UzmanlıkGüncelleDto
+    {
+        public int UzmanlıkId { get; set; }
+        public string UzmanlıkAdı { get; set; }
+    }
+}
